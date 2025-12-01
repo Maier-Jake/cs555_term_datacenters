0783f386bf668a9b60e4657e8b4766f6153a22a6
Jake Maier <jakedmaier@gmail.com>
2025-11-30 18:50:49 -0700

    Full data ready to train

--- src/main/scala/DataCenterPrices/Main.scala
+++ src/main/scala/DataCenterPrices/Main.scala
@@ -1,9 +1,10 @@
 package DataCenterPrices
 
+import org.apache.spark.sql.Column
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.functions._
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types.{StructType, StructField, DoubleType, StringType, IntegerType}
-import org.apache.spark.sql.functions._
-import org.apache.spark.sql.Column
 import org.apache.spark.ml.evaluation.RegressionEvaluator
 import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.regression.GBTRegressor
@@ -33,8 +34,8 @@ object Main {
       .add("Utility_Name", StringType)
       .add("Latitude", DoubleType)
       .add("Longitude", DoubleType)
-      .add("Opening_Year", IntegerType)
-      .add("Opening_Month", IntegerType)
+      .add("Opening_Year", DoubleType)
+      .add("Opening_Month", DoubleType)
       .add("Capacity_MW", DoubleType)
       .add("Verified", StringType)
       .add("Data_Source", StringType)
@@ -44,13 +45,15 @@ object Main {
       .option("header", "true")
       .schema(dataCenterSchema)
       .csv(args(0) + "/data_centers.csv")
+      .na.fill(0.0, Seq("Capacity_MW")) // fill in the NULLs for now until Eric fills them in in the database
+      .withColumn("Opening_Year", $"Opening_Year".cast("int"))
+      .withColumn("Opening_Month", $"Opening_Month".cast("int"))
       .drop("Notes", "Source_URL", "Data_Source", "Verified",
         "Opening_Month", "Latitude", "Longitude")
+    // println(s"Loaded Data Centers: ${dataCenters.count()} rows")
 
-    println(s"Loaded Data Centers: ${dataCenters.count()} rows")
-
-    println("Preview of dataCenters:")
-    dataCenters.show(10, truncate = false)
+    // println("Preview of dataCenters:")
+    // dataCenters.show(10, truncate = false)
 
     // Step 3: Load the power_costs data
     val powerSchema = new StructType()
@@ -73,25 +76,74 @@ object Main {
        * customer class but that split is not defined in the data */
       // Cleanup data, removing adjustment ids, converting strings to ints where appropriate
       .filter(!$"Utility_ID".isin("99999.0", "88888.0", "77777.0"))
-      .withColumn(
-        "Utility_ID",
-        regexp_replace($"Utility_ID", "\\.0$", "").cast(IntegerType)
-      )
-      .withColumn(
-        "Customers",
-        regexp_replace($"Customers", "\\.0$", "").cast(IntegerType)
-      )
-    println(s"Loaded Power Costs: ${powerCosts.count()} rows")
+      .withColumn("Utility_ID", col("Utility_ID").cast("double").cast("int"))
+      .withColumn("Customers", col("Customers").cast("double").cast("int"))
+      .cache()
+    // println(s"Loaded Power Costs: ${powerCosts.count()} rows")
 
-    println("Preview of powerCosts:")
-    powerCosts.show(10, truncate = false)
+    // println("Preview of powerCosts:")
+    // powerCosts.show(10, truncate = false)
 
     // Step 4: Feed into the training of the model
     // Model Goal: Determine if the opening of data centers is causing residential energy prices to increase.
-    // We need te able to feed "theoretical" data center openings to see how the state might raise their residential energy prices.
+    // We need the able to feed "theoretical" data center openings to see how the state might raise their residential energy prices.
 
+    val minYear = powerCosts.agg(min("Year")).as[Int].first()
+    val maxYear = powerCosts.agg(max("Year")).as[Int].first()
+    println(s"Year range: $minYear to $maxYear")
+    val years = spark.range(minYear, maxYear + 1).withColumnRenamed("id", "Year")
+    // Yeah, there is probably an issue here if we get states that don't have any datacenters. Like idk I'm gonna guess mississippi and or louisiana don't have any lol
+    val states = dataCenters.select("State").distinct()
+    val stateYearGrid = states.crossJoin(years)
 
+    states.show(52)
 
+    // Get a dataframe with the datacenters grouped by the state they are in and the year they are opened.
+    val dcByStateYear = dataCenters
+      .groupBy($"State", $"Opening_Year")
+      .agg(
+        count("*").alias("Num_DataCenters_Opened"),
+        sum($"Capacity_MW").alias("Total_Capacity_Added")
+      )
+      .withColumnRenamed("Opening_Year", "Year")
+    
+    // println("Preview of dcByStateYear:")
+    // dcByStateYear.show(20, truncate = false)
+
+    // Now I need to join my overall stateYearGrid with the datacenter data
+    val w = Window.partitionBy("State").orderBy("Year")
+
+    val dcFull = stateYearGrid
+      .join(dcByStateYear, Seq("State", "Year"), "left")
+      .na.fill(0, Seq("Num_DataCenters_Opened", "Total_Capacity_Added"))
+      .withColumn("Cumulative_DataCenters",
+        sum($"Num_DataCenters_Opened").over(w)
+      )
+      .withColumn("Cumulative_Capacity_MW",
+        sum($"Total_Capacity_Added").over(w)
+      )
+      .drop("Num_DataCenters_Opened", "Total_Capacity_Added")
+
+    // println("Preview of dcFull:")
+    // dcFull.filter($"State" === "VA")
+    //   .orderBy("Year")
+    //   .show(20, truncate = false)
+
+    // Now we need to get the average electricity price for each state for each year and add that as columns to dcFull
+    val stateYearPrices = powerCosts
+      .groupBy("State", "Year")
+      .agg(
+        avg("Price_Per_kWh").alias("Avg_Residential_kWh")
+      )
+
+    val fullPanel = dcFull
+      .join(stateYearPrices, Seq("State", "Year"), "left")
+      .na.drop("any", Seq("Avg_Residential_kWh"))
+
+    // Any data center that opened before minYear in EIA is just part of the starting conditions of the grid.
+    // We will not estimate its opening shock... we will just treat it as baseline load.
+
+    fullPanel.show()
     spark.stop()
 
   }

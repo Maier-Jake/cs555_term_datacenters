49a433021317ddee4088055b434459f5af49f90d
Jake Maier <jakedmaier@gmail.com>
2025-11-30 18:54:44 -0700

    I don't need to specify client, that is default

--- run.sh
+++ run.sh
@@ -10,10 +10,10 @@ sbt clean package
 # Step 2: Submit the Spark job
 echo "Submitting the Spark job..."
 
+# You can add --deploy-mode cluster \
 spark-submit \
   --class DataCenterPrices.Main \
   --master yarn \
-  --deploy-mode client \
   --conf "spark.driver.extraJavaOptions=-Dlog4j.configurationFile=log4j2.properties" \
   --conf "spark.executor.extraJavaOptions=-Dlog4j.configurationFile=log4j2.properties" \
   target/scala-2.12/datacenterprices_2.12-0.1.0-SNAPSHOT.jar \
